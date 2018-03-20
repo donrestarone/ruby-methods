694e2b5298276cf6a469f97d16792cdb06fd160c
donrestarone <shashikejayatunge@gmail.com>
2018-03-20 18:05:40 -0400

    add logic and tested

--- exercise8.rb
+++ exercise8.rb
@@ -1,9 +1,19 @@
 #distance is in meters
 #time in minutes
-p "enter distance in meters"
+p "enter distance in meters person 1"
 d = gets.chomp.to_f
-p "enter time in minutes"
+p "enter time in minutes person 1"
 t = gets.chomp.to_f
+
+p "enter distance in meters person 2"
+d1 = gets.chomp.to_f
+p "enter time in minutes person 2"
+t1 = gets.chomp.to_f
+
+p "enter distance in meters person 3"
+d2 = gets.chomp.to_f
+p "enter time in minutes person 3"
+t2 = gets.chomp.to_f
 
 def speedster(distance, time)
 	velocity = distance / (time * 60)
@@ -13,4 +23,20 @@ end
 
 v = speedster(d, t) 
 
-puts "velocity was #{v} meters per second squared"  
+ puts "velocity was #{v} meters per second squared"  
+
+speed1 = speedster(d, t)
+speed2 = speedster(d1, t1)
+speed3 = speedster(d2, t2)
+ if speed3 > speed2 && speed3 > speed1
+  puts "Person 3 was the fastest at #{speed3} m/s"
+elsif speed2 > speed3 && speed2 > speed1
+  puts "Person 2 was the fastest at #{speed2} m/s"
+elsif speed1 > speed3 && speed1 > speed2
+  puts "Person 1 was the fastest at #{speed1} m/s"
+elsif speed1 == speed2 && speed2 == speed3
+  puts "Everyone tied at #{speed1} m/s"
+else
+  puts "Well done everyone!"
+end
+
